83f2985d1c316bf76ec61c5f52794170f5aac51c
Andrii Omelkovets <Andrii Omelkovets>
2016-08-16 02:00:22 +0300

    push project

---
+++ js/controller/controller.js
@@ -0,0 +1,35 @@
+﻿(function () {
+  'use strict';
+
+  angular
+      .module('app')
+      .controller('controller', controller);
+
+  controller.$inject = ['factory'];
+
+  function controller(factory) {
+    /* jshint validthis:true */
+    var vm = this;
+    vm.books = [];
+    vm.index = null;
+    vm.showWindow = false;
+    vm.statusError = false;
+    vm.statusOk = false;
+    vm.title = "Demo application Title";
+
+
+    factory.getData().then(function (data) {
+      if (data.status == 200) {
+        vm.books = data.data;
+        vm.statusOk = true;
+      } else {
+        vm.statusError = true;
+      }
+    });
+
+    vm.showModal = function (id) {
+      vm.index = id;
+      vm.showWindow = true;
+    }
+  }
+})();
---
+++ js/services/factory.js
@@ -0,0 +1,32 @@
+﻿(function () {
+  'use strict';
+
+  angular
+      .module('app')
+      .factory('factory', factory);
+
+  factory.$inject = ['$http', '$q'];
+
+  function factory($http, $q) {
+    var service = {
+      getData: getData
+    };
+
+    return service;
+
+    function getData() {
+      var q = $q.defer();
+      $http({
+        method: "GET",
+        url: 'http://cm.mmi.macc.com.ua/tests/sample.php'
+      }).then(handleSuccess, handleError);
+      function handleSuccess(response) {
+        q.resolve(response);
+      };
+      function handleError(error) {
+        q.resolve(error);
+      };
+      return q.promise;
+    }
+  }
+})();
